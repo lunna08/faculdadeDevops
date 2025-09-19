102067b4617ea38d6cbdac2482e443f9f50bce0d
Lunna Brenda <lunnabrendaazevedo@gmail.com>
2025-09-19 20:43:56 -0300

    Update tests_quiz.py

--- tests/tests_quiz.py
+++ tests/tests_quiz.py
@@ -8,28 +8,28 @@ from src.questoes import perguntas
 
 class TestQuiz(unittest.TestCase):
 
-    @patch("builtins.input", side_effect=["2", "2", "1"])  # todas corretas
+    @patch("builtins.input", side_effect=["2", "2", "1"])
     @patch("sys.stdout", new_callable=StringIO)
     def test_quiz_todas_corretas(self, mock_stdout, mock_input):
         executar_quiz(perguntas)
         output = mock_stdout.getvalue()
         self.assertIn("Você fez 3 ponto(s) de 3.", output)
 
-    @patch("builtins.input", side_effect=["1", "1", "1"])  # só a última correta
+    @patch("builtins.input", side_effect=["1", "1", "1"])
     @patch("sys.stdout", new_callable=StringIO)
     def test_quiz_algumas_erradas(self, mock_stdout, mock_input):
         executar_quiz(perguntas)
         output = mock_stdout.getvalue()
         self.assertIn("Você fez 1 ponto(s) de 3.", output)
 
-    @patch("builtins.input", side_effect=["3", "1", "3"])  # todas erradas
+    @patch("builtins.input", side_effect=["3", "1", "3"])
     @patch("sys.stdout", new_callable=StringIO)
     def test_quiz_todas_erradas(self, mock_stdout, mock_input):
         executar_quiz(perguntas)
         output = mock_stdout.getvalue()
         self.assertIn("Você fez 0 ponto(s) de 3.", output)
 
-    @patch("builtins.input", side_effect=["2", "2", "1"])  # todas corretas
+    @patch("builtins.input", side_effect=["2", "2", "1"])
     @patch("sys.stdout", new_callable=StringIO)
     def test_mensagens_corretas(self, mock_stdout, mock_input):
         executar_quiz(perguntas)
@@ -37,7 +37,7 @@ class TestQuiz(unittest.TestCase):
         self.assertIn("Correto!", output)
         self.assertNotIn("Errado!", output)
 
-    @patch("builtins.input", side_effect=["3", "1", "3"])  # todas erradas
+    @patch("builtins.input", side_effect=["3", "1", "3"])
     @patch("sys.stdout", new_callable=StringIO)
     def test_mensagens_erradas(self, mock_stdout, mock_input):
         executar_quiz(perguntas)
